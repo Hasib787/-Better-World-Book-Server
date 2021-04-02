554445299d9f86658489ff6630a8c1cf60574871
Hasib787 <mr.hasib45@gmail.com>
2021-04-02 23:21:33 +0600

    added some chnages

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config()
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fg2cz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority` ;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fg2cz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express()
-
+app.use(bodyParser.json());
+app.use(cors());
 
 client.connect(err => {
-  const collection = client.db("bookShop").collection("books");
-    console.log("Database are connected")
+    const booksCollection = client.db("bookShop").collection("books");
+
+    app.post('/addBooks', (req, res) => {
+        res.send('books are added')
+    })
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(process.env.PORT || 5000)
